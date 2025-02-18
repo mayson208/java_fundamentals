968d44ede621a7d37018e02d4484d3819f34d142
mayson208 <chudworth0@gmail.com>
2025-02-18 10:21:56 -0500

    fixed error in infiniteloop.java file

--- InfiniteLoop.java
+++ InfiniteLoop.java
@@ -1,4 +1,4 @@
-public class InfiniteLoop.java {
+public class InfiniteLoop {
     public static void main(String[] args) {
         for (;;) {
             System.out.println("This is an infinite loop!");
